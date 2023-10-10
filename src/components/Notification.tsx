94bf653a435fa0b70021f11e682c7d1d1195ca86
samin <samincgs@gmail.com>
2023-10-10 19:42:05 -0400

    Created Notification component for home page

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const Notification = () => {
-  return <div>Notification</div>;
+  return (
+    <div className="h-12 bg-red-500 text-white text-center text-sm md:text-base px-4 flex items-center justify-center cursor-pointer">
+      Free delivery for all orders over $50. Order your food now!
+    </div>
+  );
 };
 
 export default Notification;
