9c8e2ca798165c8bd779ab76e79909695e278f66
samin <samincgs@gmail.com>
2023-10-11 23:47:02 -0400

    Will now work on the rest of the home page which is the Slider for the food, Featured for users can see the fav items and countdown component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
+import Featured from "@/components/Featured";
+import Offer from "@/components/Offer";
+import Slider from "@/components/Slider";
 import Image from "next/image";
 
 export default function Home() {
-  return <main>hello world!</main>;
+  return (
+    <main>
+      <Slider />
+      <Featured />
+      <Offer />
+    </main>
+  );
 }
