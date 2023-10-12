a74da7b5937103c69cb487556d9933e7bdb1f55f
samin <samincgs@gmail.com>
2023-10-12 19:05:38 -0400

    Created all the UI for the Slider for both the mobile and the desktop

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -10,7 +10,7 @@ const data = [
   },
   {
     id: 2,
-    title: "we deliver your order wherever you are in NY",
+    title: "we deliver your order wherever you are",
     image: "/slide2.png",
   },
   {
@@ -21,6 +21,35 @@ const data = [
 ];
 
 const Slider = () => {
-  return <div>Slider</div>;
+  const [currentSlide, setCurrentSlide] = useState(1);
+
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     setCurrentSlide((prev) => (prev == data.length - 1 ? 0 : prev + 1));
+  //   }, 2000);
+
+  //   return () => clearInterval(interval);
+  // }, []);
+
+  return (
+    <div className="flex flex-col h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] lg:flex-row">
+      {/* Text Container */}
+      <div className="flex-1 flex items-center justify-center flex-col gap-8 text-red-500 font-bold bg-fuchsia-50">
+        <h1 className="text-5xl text-center uppercase p-4 md:p-10 md:text-6xl xl:text-7xl">
+          {data[currentSlide].title}
+        </h1>
+        <button className="bg-red-500 text-white py-4 px-8">Order Now</button>
+      </div>
+      {/* Image Container */}
+      <div className="w-full flex-1 relative">
+        <Image
+          src={data[currentSlide].image}
+          alt=""
+          fill
+          className=" object-cover"
+        />
+      </div>
+    </div>
+  );
 };
 export default Slider;
