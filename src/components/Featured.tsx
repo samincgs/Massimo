acd6fee4ab2107cd5849a8b329661597bebec3bc
samin <samincgs@gmail.com>
2023-10-12 21:34:03 -0400

    Working on featured component which scrollable UI and food items with animation

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -1,7 +1,28 @@
+import Image from "next/image";
 import React from "react";
 
 const Featured = () => {
-  return <div>Featured</div>;
+  return (
+    <div className="w-screen overflow-x-scroll text-red-500">
+      {/* Wrapper */}
+      <div className=" w-max flex">
+        {/* Single Item */}
+        <div className="w-screen h-[60vh] flex flex-col items-center justify-around">
+          {/* Image Container */}
+          <div className="relative">
+            <Image src="" alt="" fill />
+          </div>
+          {/* Text Container */}
+          <div className="">
+            <h1 className="">Title</h1>
+            <p className="">Desc</p>
+            <span className="">Price</span>
+            <button>Add to Cart</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Featured;
--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -5,17 +5,17 @@ import React, { useEffect, useState } from "react";
 const data = [
   {
     id: 1,
-    title: "always fresh & always crispy & always hot",
+    title: "Deliciousness, right to your doorstep",
     image: "/slide1.png",
   },
   {
     id: 2,
-    title: "we deliver your order wherever you are",
+    title: "We deliver your order wherever you are",
     image: "/slide2.png",
   },
   {
     id: 3,
-    title: "the best pizza to share with your family",
+    title: "The best pizza to share with your family",
     image: "/slide3.jpg",
   },
 ];
@@ -35,7 +35,7 @@ const Slider = () => {
     <div className="flex flex-col h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] lg:flex-row">
       {/* Text Container */}
       <div className="flex-1 flex items-center justify-center flex-col gap-8 text-red-500 font-bold bg-fuchsia-50">
-        <h1 className="text-5xl text-center uppercase p-4 md:p-10 md:text-6xl xl:text-7xl">
+        <h1 className="text-5xl text-center p-4 md:p-10 md:text-6xl xl:text-7xl">
           {data[currentSlide].title}
         </h1>
         <button className="bg-red-500 text-white py-4 px-8">Order Now</button>
