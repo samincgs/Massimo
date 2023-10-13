455470eca15c67dcb5376f44558e2067b71c0e83
samin <samincgs@gmail.com>
2023-10-12 23:27:27 -0400

    Fully implemented the UI for the featured component for both mobile and desktop users

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -4,14 +4,14 @@ import React from "react";
 
 const Featured = () => {
   return (
-    <div className="w-screen overflow-x-scroll text-red-500">
+    <div className=" overflow-x-scroll text-red-500">
       {/* Wrapper */}
-      <div className=" w-max flex">
+      <div className="w-max flex">
         {/* Single Item */}
         {featuredProducts.map((item) => (
           <div
             key={item.id}
-            className="w-screen h-[60vh] flex flex-col items-center justify-around p-4 hover:bg-fuchsia-50 transition-all duration-300"
+            className="w-screen h-[60vh] flex flex-col items-center justify-around p-4 hover:bg-fuchsia-50 transition-all duration-300 md:w-[50vw] xl:w-[33vw] xl:[90vh]"
           >
             {/* Image Container */}
             {item.img && (
@@ -20,9 +20,11 @@ const Featured = () => {
               </div>
             )}
             {/* Text Container */}
-            <div className="flex-1 flex flex-col gap-3 items-center text-center">
-              <h1 className=" text-xl font-bold uppercase">{item.title}</h1>
-              <p className="p-5">{item.desc}</p>
+            <div className="flex-1 flex flex-col gap-3 items-center text-center justify-center">
+              <h1 className=" text-xl font-bold uppercase xl:text-2xl 2xl:text-3xl">
+                {item.title}
+              </h1>
+              <p className="p-4 2xl:p-8">{item.desc}</p>
               <span className=" text-xl font-bold">${item.price}</span>
               <button className="bg-red-500 text-white p-2 rounded-md">
                 Add to Cart
