44949096079fbde664e0cfcca8f3f5d23dd17a5f
samin <samincgs@gmail.com>
2023-10-13 16:40:45 -0400

    Fully finished the Offer page for for mobile and desktop users meaning the Home page is finally completed

--- src/components/CountDown.tsx
+++ src/components/CountDown.tsx
@@ -2,11 +2,13 @@
 import React from "react";
 import Countdown from "react-countdown";
 
+const endingDate = new Date("2023-10-25");
+
 const CountDown = () => {
   return (
     <Countdown
-      date={new Date("2023-10-25")}
       className="font-bold text-5xl text-yellow-300"
+      date={endingDate}
     />
   );
 };
--- src/components/Offer.tsx
+++ src/components/Offer.tsx
@@ -4,7 +4,7 @@ import CountDown from "./CountDown";
 
 const Offer = () => {
   return (
-    <div className="bg-black h-screen flex flex-col md:flex-row">
+    <div className="bg-black h-screen flex flex-col md:flex-row md:justify-between md:bg-[url('/offerBg.png')] md:h-[70vh]">
       {/* Text Container */}
       <div className="flex-1 text-white flex flex-col justify-center items-center text-center gap-8 p-6">
         <h1 className="text-5xl font-bold xl:text-6xl">
@@ -20,7 +20,7 @@ const Offer = () => {
         </button>
       </div>
       {/* Image Container */}
-      <div className="flex-1 w-full relative">
+      <div className="flex-1 w-full relative md:h-full">
         <Image
           src="/offerProduct.png"
           alt=""
