8fbbb4c90bbecb538999270910f361e8f41a5260
samin <samincgs@gmail.com>
2023-10-13 14:25:44 -0400

    Implementing the offer component for website home page

--- src/components/Offer.tsx
+++ src/components/Offer.tsx
@@ -1,7 +1,33 @@
+import Image from "next/image";
 import React from "react";
 
 const Offer = () => {
-  return <div>Offer</div>;
+  return (
+    <div className="bg-black h-screen flex flex-col md:flex-row">
+      {/* Text Container */}
+      <div className="flex-1 text-white flex flex-col justify-center items-center text-center gap-8 p-6">
+        <h1 className="text-5xl font-bold xl:text-6xl">
+          Delicious Burger & Frency Fries
+        </h1>
+        <p className="xl:text-xl">
+          Progressively simplify effectiver e-toilers and process-centric method
+          of empowerment. Quickly pontificate parallel.
+        </p>
+        <button className="bg-red-500 text-white rounded-md py-3 px-6">
+          Order Now
+        </button>
+      </div>
+      {/* Image Container */}
+      <div className="flex-1 w-full relative">
+        <Image
+          src="/offerProduct.png"
+          alt=""
+          fill
+          className=" object-contain"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Offer;
