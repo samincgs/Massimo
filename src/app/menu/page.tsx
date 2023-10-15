29510f4d77115cc84fcf3122670af92e2991af0b
samin <samincgs@gmail.com>
2023-10-15 17:10:20 -0400

    Still working on menu page

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -3,7 +3,10 @@ import React from "react";
 const MenuPage = () => {
   return (
     <div>
+      {/* Text Container */}
       <div>Menu items</div>
+      {/* Image Container */}
+      <div>Images</div>
     </div>
   );
 };
